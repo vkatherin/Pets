27388247986d04285075a08af89a433b3e00bbaa
Kathe <8.3criollovanesa@gmail.com>
2024-05-03 12:49:20 -0500

    segundo corte

--- src/backend/signup.php
+++ src/backend/signup.php
@@ -1,21 +1,37 @@
 <?php
-  include('../../config/database.php');
+    include('../../config/database.php');
 
-  $fullname = $_POST['fname'];
-  $email = $_POST['email'];
-  $passwd = $_POST['passwd'];
-  $enc_pass = md5($passwd);
+    $fullname = $_POST['fname'];
+    $email = $_POST['email'];
+    $passwd = $_POST['passwd'];
+    $enc_pass = md5($passwd);
 
- $sql = "
- INSERT INTO users (fullname, email, password)
- VALUES('$fullname', '$email', '$password')
- ";
- $ans = pg_query($conn,$sql);
- if($ans){
-  echo "user has been created successfully";
- }else{
-  echo "Error:" . preg_last_error();
- }
+    $sql_validate_email = "SELECT * FROM users WHERE email = '$email'";
+    $result = pg_query($conn, $sql_validate_email);
+    $total = pg_num_rows($result);
 
+    if ($total > 0){
+      echo "<script>alert('Email already exists')</script>";
+      header("refresh:0;url=../index.html");
+    }else{
+       $sql = "
+        INSERT INTO users (fullname, email, password) 
+            VALUES ('$fullname', '$email','$enc_pass')
+    ";
 
+    $ans = pg_query($conn,$sql);
+    if ($ans){
+      echo "<script>alert('User has been registered')</script>";
+      header("refresh:0;url=../index.html");
+      
+    }else{
+        
+        echo "Error: " . pg_last_error();
+    }
+
+  }
+    //Close connection
+    pg_close($conn)
+
+    
 ?>
